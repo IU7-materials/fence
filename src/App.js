76599beed7df96d367663d26c9dca8dc48cc7024
Sergey Malikov <wind.78@mail.ru>
2023-03-27 22:30:51 +0300

    mail, telephone, start page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Contacts from "./Contacts";
+
+
 
 function App() {
+  const [page, setPage] = useState(1);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {page === 1 && (
+        <div className="page1">
+          <div className="modal">
+            <div><img src="https://hgwipn3sa-res.cloudinary.com/image/upload/w_auto,f_auto,q_auto,g_auto,c_fill,h_650,dpr_2/wkb2zkbhb4filwpsjzv1.jpg"/></div>
+            <div>
+              <h2>Пройдите тест за 2 минуты и узнайте стоимость забора</h2>
+              <div>Опыт работы 15+ лет</div>
+              <button onClick={() => setPage(2)}>Рассчитать стоимость</button>
+            </div>
+          </div>
+        </div>
+      )}
+      
+      {page === 2 && (
+        <div>
+          <div className="page2">Общая длина вашего забора, включая ворота и калитки</div>
+          <Contacts/>
+          <Mail/>
+        </div>
+      )}
     </div>
   );
 }
+
+function Mail() {
+  return (
+    <div>
+      <h3>Укажите свою почту</h3>
+      <input type="text"/> 
+    </div>
+  );
+}
+
+
 
 export default App;
---
+++ src/Contacts.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+
+
+function Contacts() {
+  return (
+    <div>
+      <h3>Укажите свой телефон</h3>
+      <input type="text"/> 
+    </div>
+  );
+}
+
+
+
+export default Contacts;
